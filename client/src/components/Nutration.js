614e2a9b97728aac5f57f24338eddc6711f1ffb3
BIBHAB22 <bibhabindia@gmail.com>
2023-07-11 20:05:01 +0530

    about us page and the designs of gym,nutrition and yoga

--- client/src/components/Nutration.js
+++ client/src/components/Nutration.js
@@ -1,12 +1,48 @@
 import React from 'react'
 import Navbar from "./Navbar";
+import "./Nutration.css";
 import 'bootstrap/dist/css/bootstrap.css';
-const Nutration = () => {
+const Nutrition = () => {
   return (
-    <div>
+      <div className='Nutrition_bg'>
       <Navbar/>
+      <div className="container_Nutrition">
+      <section className="section_Nutrition basic">
+        <h2 className="section_title_Nutrition">Basic Nutrition</h2>
+        <p className="section_description_Nutrition">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin dui eu lorem aliquam volutpat.</p>
+        <button className="section_button_Nutrition">Start Nutritionitation</button>
+      </section>
+        <section className="section_Nutrition"></section>
+        <section className="section_Nutrition"></section>
+        <section className="section_Nutrition music_section_Nutrition">
+          <h2 className="section_title_Nutrition">Music</h2>
+          {/* Music content */}
+        </section>
+        <section className="section_Nutrition program_section_Nutrition">
+          <h2 className="section_title_Nutrition">Programs</h2>
+          <div className="program_cards_row_Nutrition">
+            <div className="program_card_Nutrition"></div>
+            <div className="program_card_Nutrition"></div>
+            <div className="program_card_Nutrition"></div>
+          </div>
+          <div className="program_cards_row_Nutrition">
+            <div className="program_card_Nutrition"></div>
+            <div className="program_card_Nutrition"></div>
+            <div className="program_card_Nutrition"></div>
+          </div>
+          {/* Repeat the above rows for additional rows */}
+        </section>
+        <section className="section_Nutrition mentor_section_Nutrition">
+          <h2 className="section_title_Nutrition">Mentors</h2>
+          <div className="mentor_cards_row_Nutrition">
+            <div className="mentor_card_Nutrition"></div>
+            <div className="mentor_card_Nutrition"></div>
+            <div className="mentor_card_Nutrition"></div>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
 
-export default Nutration
+export default Nutrition
