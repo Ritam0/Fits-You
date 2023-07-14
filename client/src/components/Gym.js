df160984fba269ac4ccb6e990e713eb9523c117c
BIBHAB22 <bibhabindia@gmail.com>
2023-07-14 10:35:37 +0530

    Gym page deploy

--- client/src/components/Gym.js
+++ client/src/components/Gym.js
@@ -9,36 +9,113 @@ const Gym = () => {
       <div className="container_Gym">
       <section className="section_Gym basic">
         <h1 className="section_title_Gym">Gym Tranning</h1>
-        <h3 className="section_description_Gym">How’d you like to go on a long romantic walk on the treadmill?</h3>
+        <p className="section_description_Gym">How’d you like to go on a long romantic walk on the treadmill?</p>
         <button className="section_button_Gym">Start Your Training</button>
       </section>
-        <section className="section_Gym"></section>
-        <section className="section_Gym"></section>
-        <section className="section_Gym music_section_Gym">
-          <h2 className="section_title_Gym">Music</h2>
-          {/* Music content */}
+      <section className="section_Gym part_section_Gym">
+          <h2 className="section_title_Gym">Here are the Top Benefits of Gym:</h2>
+          <ul>
+            <li>Regular gym workouts contribute to improved physical health. They help in building strength, increasing endurance, enhancing flexibility, and improving cardiovascular fitness. Exercise can also help in managing weight, reducing the risk of chronic diseases such as obesity, diabetes, and heart disease.</li>
+            <li>Gym workouts involve strength training exercises that target various muscle groups. These exercises stimulate muscle growth, leading to increased muscle strength and tone. Stronger muscles provide better support for joints and bones, reducing the risk of injury.</li>
+            <li>Gym workouts are an effective way to manage weight. Combining cardiovascular exercises with strength training helps in burning calories and building lean muscle mass. Regular exercise boosts metabolism, which can aid in weight loss and weight maintenance.</li>
+            <li>Exercise is known to have positive effects on mental health. Gym workouts release endorphins, which are natural mood-boosting chemicals in the brain. Regular exercise can reduce stress, anxiety, and symptoms of depression. It can also improve sleep quality and enhance overall cognitive function.</li>
+            <li>Engaging in regular physical activity at the gym can increase energy levels and combat fatigue. Exercise stimulates blood flow, improving oxygen and nutrient delivery to the body's tissues. This, in turn, enhances energy production and reduces feelings of tiredness.</li>
+          </ul>
+      </section>
+      <section className="section_Gym program_section_Gym">
+          <h2 className="section_title_Gym">Bisecps And Triceps Workouts</h2>
+          <div className="program_cards_row_Gym">
+            <div className="program_card_Gym">
+              <div className="divimg_Gym">
+                  <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTRp9VeuV-aTA8BllbhFE2BffZyPoJO5Cgz1YO1ivttB_lH1NAl" alt="img"/>
+                  <h3>Concentration curl</h3>
+                  <h4> To do a concentration curl:</h4>
+
+                 <p>1. Sit at the end of a flat bench with your legs open in a V shape.</p>
+                 <p>2. Grip a dumbbell with one hand and lean forward slightly.</p>
+                 <p>3. With your palm facing your center, rest your elbow against the inside of your thigh.</p>
+                 <p>4. Rest your other hand or elbow on the other thigh for stability.</p>
+                 <p>5. While keeping your upper body still, curl the weight slowly toward your shoulder.</p>
+                 <p>6. As you lift, turn your wrist slightly so that you end the curl with your palm facing your shoulder.</p>
+                 <p>7. Pause for a moment, allowing yourself to feel the effort in your bicep, and then slowly lower the weight. Don’t rest it on the floor, though, until your final repetition.</p>
+                 <p>8. Repeat 12 to 15 times, then switch arms.</p>
+              </div>
+            </div>
+
+            <div className="program_card_Gym">
+              <div className="divimg_Gym">
+                  <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS3dnPv8jZ-bZBMqXRxV3E2VZ8Hf0UMBXdMSRAtPIUeiHaJnUlC" alt="img"/>
+                  <h3>Cable curl</h3>
+                  <h4>For a standing one-arm cable curl:</h4>
+                  <p>1.Stand a couple of feet from the pulley machine, and grasp the cable handle with your palm facing forward and your elbow close to your side.</p>
+                  <p>2.Place the foot opposite your curling hand a little in front of your other foot for better balance.</p>
+                  <p>3.Slowly curl your arm, bringing your palm toward your shoulder.</p>
+                  <p>4.Hold the curl up for a moment and feel the exertion in your bicep.</p>
+                  <p>5.Slowly lower the handle to the starting position.</p>
+                  <p>6.Do 12 to 15 repetitions, then switch arms.</p>
+                  
+              </div>
+            </div>
+
+            <div className="program_card_Gym">
+              <div className="divimg_Gym">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZXGryuepRMeAUvlo7OoCQgtWetUlTXDl0-S_R8D4iscuaUExm" alt="img" />
+                  <h3>Barbell curl</h3>
+                  <h4>To do a barbell curl: </h4>
+                  <p>1.Stand with your feet about shoulder-width apart.</p>
+                  <p>2.Hold the barbell with your arms at your side, palms facing out.</p>
+                  <p>3.While exhaling, slowly curl the barbell up toward your chest. Keep your chest still, using only your arms to lift the barbell.</p>
+                  <p>4.Hold the position for a second, then slowly lower the barbell to its starting position.</p>
+                  <p>5.Repeat 12 to 15 times.</p>
+              </div>
+            </div>
+            </div>
         </section>
         <section className="section_Gym program_section_Gym">
-          <h2 className="section_title_Gym">Programs</h2>
+          <h2 className="section_title_Gym">Chest Workouts</h2>
           <div className="program_cards_row_Gym">
-            <div className="program_card_Gym"></div>
-            <div className="program_card_Gym"></div>
-            <div className="program_card_Gym"></div>
-          </div>
-          <div className="program_cards_row_Gym">
-            <div className="program_card_Gym"></div>
-            <div className="program_card_Gym"></div>
-            <div className="program_card_Gym"></div>
-          </div>
-          {/* Repeat the above rows for additional rows */}
-        </section>
-        <section className="section_Gym mentor_section_Gym">
-          <h2 className="section_title_Gym">Mentors</h2>
-          <div className="mentor_cards_row_Gym">
-            <div className="mentor_card_Gym"></div>
-            <div className="mentor_card_Gym"></div>
-            <div className="mentor_card_Gym"></div>
-          </div>
+            <div className="program_card_Gym">
+              <div className="divimg_Gym">
+                  <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTRp9VeuV-aTA8BllbhFE2BffZyPoJO5Cgz1YO1ivttB_lH1NAl" alt="img"/>
+                  <h3>Incline push up</h3>
+                  <h4>Equipment required: None</h4>
+                  <p> This is a good warmup to prepare the chest for work. Research has shown that a dynamic warmup is helpful in preventing injury prior to training. Lower resistance movements related to those you are about to perform prepares the muscles for work.</p>
+
+                 <p>1. Start with your hands on the wall or a countertop height surface. Walk your feet back so that your body makes roughly a 45 degree angle with the floor.</p>
+                 <p>2. Keep your body straight and your spine neutral, and lower your chest to the surface you’re leaning against.</p>
+                 <p>3. Pause for a moment, then return to the start position</p>
+                 <p>4. Make sure the resistance feels light enough to complete up to 20 repetitions. If you need to make it easier, step closer to your hands; to make it harder, step farther away.</p>
+              </div>
+            </div>
+
+            <div className="program_card_Gym">
+              <div className="divimg_Gym">
+                  <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS3dnPv8jZ-bZBMqXRxV3E2VZ8Hf0UMBXdMSRAtPIUeiHaJnUlC" alt="img"/>
+                  <h3>Decline bench press</h3>
+                  <h4>Equipment required: barbell, decline bench</h4>
+                  <p>1.Stand a couple of feet from the pulley machine, and grasp the cable handle with your palm facing forward and your elbow close to your side.</p>
+                  <p>2.Place the foot opposite your curling hand a little in front of your other foot for better balance.</p>
+                  <p>3.Slowly curl your arm, bringing your palm toward your shoulder.</p>
+                  <p>4.Hold the curl up for a moment and feel the exertion in your bicep.</p>
+                  <p>5.Slowly lower the handle to the starting position.</p>
+                  <p>6.Do 12 to 15 repetitions, then switch arms.</p>
+                  
+              </div>
+            </div>
+
+            <div className="program_card_Gym">
+              <div className="divimg_Gym">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZXGryuepRMeAUvlo7OoCQgtWetUlTXDl0-S_R8D4iscuaUExm" alt="img" />
+                  <h3>Barbell curl</h3>
+                  <h3>To do a barbell curl: </h3>
+                  <p>1.Stand with your feet about shoulder-width apart.</p>
+                  <p>2.Hold the barbell with your arms at your side, palms facing out.</p>
+                  <p>3.While exhaling, slowly curl the barbell up toward your chest. Keep your chest still, using only your arms to lift the barbell.</p>
+                  <p>4.Hold the position for a second, then slowly lower the barbell to its starting position.</p>
+                  <p>5.Repeat 12 to 15 times.</p>
+              </div>
+            </div>
+            </div>
         </section>
       </div>
     </div>
