c66d01db16890df859ad07b6409eae3630c261b8
Ritam Majumder <ritmmajumder0@gmail.com>
2023-07-16 11:41:58 +0530

    fitsyou

--- server/app.js
+++ server/app.js
@@ -5,18 +5,6 @@ const databaseConnect=require('./config/dbConfig')
 const cookieparser=require('cookie-parser')
 
 
-const path=require('path');
-
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
-
 
 
 databaseConnect();
